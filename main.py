d2b3240f631d350b05a3a83717b96fd7d96bde55
skilaren <maksvenberv@yandex.ru>
2021-04-22 23:03:28 +0300

    Update

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import json
 import pprint
 import subprocess
 
+from requests.exceptions import HTTPError
+
 from csparser import DemoParser
 from player_info import get_player_info, Player
 from requester import Requester
+
 
 if __name__ == '__main__':
 
@@ -55,23 +58,29 @@ if __name__ == '__main__':
                 print(match['matchId'])
                 players = match['playingPlayers']
                 for player in players:
-                    try:
-                        player_stats = get_player_info(r.get_player_stats(player))
-                        player_details = r.get_player_details(player)
-                        player_info = Player(
-                            stats=player_stats,
-                            nickname=player_details['nickname'],
-                            rank=player_details['games']['csgo']['skill_level'],
-                            id=player,
-                            elo=player_details['games']['csgo']['faceit_elo'],
-                            faceit_id=''
-                        )
-                        res_file.write(player_info.to_csv())
-                        print(f'{player_info.nickname:20} {player_info.rank:3} {player_info.elo:5}')
-                        ranks_players[player_info.rank] += 1
-                        counter += 1
-                    except KeyError:
-                        print(f'Player "{player}" has no CS in his games')
+                    while True:
+                        try:
+                            player_stats = get_player_info(r.get_player_stats(player))
+                            player_details = r.get_player_details(player)
+                            player_info = Player(
+                                stats=player_stats,
+                                nickname=player_details['nickname'],
+                                rank=player_details['games']['csgo']['skill_level'],
+                                id=player,
+                                elo=player_details['games']['csgo']['faceit_elo'],
+                                faceit_id=''
+                            )
+                            res_file.write(player_info.to_csv())
+                            print(f'{player_info.nickname:20} {player_info.rank:3} {player_info.elo:5}')
+                            ranks_players[player_info.rank] += 1
+                            counter += 1
+                            break
+                        except KeyError:
+                            print(f'Player "{player}" has no CS in his games')
+                            break
+                        except HTTPError:
+                            print(f'HTTPError')
+                            continue
                 print(counter)
                 if counter > 10000:
                     break
